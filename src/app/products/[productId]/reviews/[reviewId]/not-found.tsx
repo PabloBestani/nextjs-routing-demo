526d8809543e27acc64d956f965db516f41f50d5
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-15 14:49:05 -0300

    feat(productReviews): added personalised not-found

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound () {
+    return <h1>Review not found in product</h1>
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail ({ params }: {
     params: {
         productId: string;
@@ -5,5 +7,7 @@ export default function ReviewDetail ({ params }: {
     }
 }) {
     const { productId, reviewId } = params;
+    const parsedRevId = parseInt(reviewId);
+    if (!parsedRevId || parsedRevId > 1000) notFound();
     return <h3>This is review {reviewId} for product {productId}</h3>
 }
