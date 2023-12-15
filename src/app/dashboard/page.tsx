fa9c93e0ae7849c325280a3813847dd08da9dce7
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-15 15:07:17 -0300

    feat(components): rendered basic components in page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import BarChart from "@/components/bar-chart";
+import LineChart from "@/components/line-chart";
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <BarChart/>
+            <LineChart/>
+        </div>
+    )
+}
